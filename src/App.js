3d9155de875857d7c3832c589d90a0fda755e77c
aditya sharma <sharmaaditya5465@gmail.com>
2025-09-06 07:15:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    
+        <div className="nav">
+    <div className="text"> ENJOY FREE SHIPPING ON ORDER ABOVE  ₹ </div>
+  </div>
+
+  <div className=" second-nav">
+
+    <div className=" left">
+      
+
+        <img src="" alt="" />
+      {/* <p>ship to = india (₹) </p> */}
+
+
     </div>
-  );
+      
+    </div>
+
+    </div>
+  )
 }
 
-export default App;
+export default App
